e879f04222988ed28245f19c7c88540836ff9b36
louxfaure <louxfaure@gmail.com>
2019-09-17 15:12:18 +0200

    test

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ import {customBriefDisplayConfig} from './prmBriefResultAfter/customBriefDisplay
 import {affDispoConfig} from './prmSearchResultAvailabilityLineAfter/affDispo'
 import {dispoElecAutresInstConfig} from './prmFullViewServiceContainerAfter/dispoElecAutresInst'
 import {itemInLoanConfig} from './prmLoanAfter/itemInLoan'
-// import {bouttonAfficheDispoElecConfig} from './prmFullViewServiceContainerAfter/bouttonAfficheDispoElec'
 
 let app = angular.module('centralCustom',[]);
 if (app){
@@ -14,7 +13,6 @@ if (app){
     app.component('prmSearchResultAvailabilityLineAfter', affDispoConfig);
     app.component('prmFullViewServiceContainerAfter', dispoElecAutresInstConfig);
     app.component('prmLoanAfter',itemInLoanConfig);
-    // app.component('rebubComponent', bouttonAfficheDispoElecConfig);
 } 
 
 // angular.module('primo-explore.config').run(['$templateCache', function($templateCache) {
